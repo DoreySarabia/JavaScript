542681371816033961ff5eec974c308023fb4a8e
DoreySarabia <n.doryse@gmail.com>
2021-09-01 17:51:30 -0500

    Objetos en JS

--- Objetos.js
+++ Objetos.js
@@ -1,5 +1,4 @@
 
-
 //Contiene propiedades y métodos
 let persona = {
     nombre: 'Nombre',
@@ -74,4 +73,164 @@ let personaString = JSON.stringify(persona);
 console.log(personaString);
 
 
+
 //M E T O D O   G E T 
+
+let persona = {
+    nombre: 'Nombre',
+    apellido: 'Apellido',
+    email: 'nombre@gmail.com',
+    edad: 28, 
+    get nombreCompleto(){
+        return this.nombre + ' ' + this.apellido;
+    }
+}
+console.log(persona.nombreCompleto);
+
+
+
+
+//M E T O D O   S E T 
+
+let persona = {
+    nombre: 'Nombre',
+    apellido: 'Apellido',
+    email: 'nombre@gmail.com',
+    edad: 28, 
+    idioma: 'es',
+    get lang(){
+        return this.idioma.toUpperCase();
+    },
+    set lang (lang){
+        this.idioma = lang.toUpperCase();
+    },
+    get nombreCompleto(){
+        return this.nombre + ' ' + this.apellido;
+    }
+}
+
+console.log(persona.lang);
+persona.lang = 'en'; 
+console.log(persona.lang);
+console.log(persona.idioma);
+
+
+
+
+
+
+//M É T O D O   C O N S T R U C T O R 
+ //Crear varios objetos   
+function Persona(nombre, apellido, email){
+    this.nombre = nombre; 
+    this.apellido = apellido; 
+    this.email = email; 
+}
+
+let padre = new Persona('Nombre1', 'Apellido1', 'email1');
+console.log(padre); 
+
+let madre = new Persona('Nombre2', 'Apellido2', 'email');
+console.log(madre);
+
+padre.nombre = 'Nombre3';
+console.log(padre); 
+
+console.log(madre);
+
+
+
+
+
+
+//A G R E G A R   M E T O D O S   A  C O N S T R U C T O R 
+
+function Persona(nombre, apellido, email){
+    this.nombre = nombre; 
+    this.apellido = apellido; 
+    this.email = email; 
+    this.nombreCompleto = function(){
+        return this.nombre +  ' '  +  this.apellido;
+    }
+}
+
+//Prototype
+Persona.prototype.tel = '5555555555';
+
+let padre = new Persona('Nombre1', 'Apellido1', 'email1');
+console.log(padre.nombreCompleto());
+padre.tel = '123456789'; 
+console.log(padre.tel); 
+
+
+let madre = new Persona('Nombre2', 'Apellido2', 'email');
+console.log(madre.nombreCompleto());
+console.log(madre.tel);
+
+
+
+
+//Quitar new sintaxis
+
+let miObjeto = new Object();
+let miObjeto2 = {};
+
+let miCadena1 = new String('Hola');
+let miCadena2 = 'Hola';
+
+let miNumero = new Number(1);
+let miNumero2 = 1;
+
+let miBoolean = new Boolean(false);
+let miBoolean2 = false;
+
+let miArreglo = new Array(); 
+let miArreglo2 = [];
+
+let miFuncion = new Function (); 
+let miFucion2 = function(){};
+
+
+
+
+
+//C A L L   E N   J S 
+let persona1 = {
+    nombre: 'Nombre1',
+    apellido: 'Apellido1', 
+    nombreCompleto: function(titulo, tel){
+        return titulo + ' : ' + this.nombre + ' ' + this.apellido + ' ' + tel;
+    }
+}
+
+let persona2 = {
+    nombre: 'Nombre2',
+    apellido: 'Apellido2'
+}
+
+//Uso de call
+console.log(persona1.nombreCompleto('Lic', '12345678'));
+
+console.log(persona1.nombreCompleto.call(persona2, 'Ingeniero', '4433221122'));
+
+
+
+//M E T O D O   A P P L Y 
+
+let persona1 = {
+    nombre: 'Nombre1',
+    apellido: 'Apellido1', 
+    nombreCompleto: function(titulo, tel){
+    return titulo + ' ' + this.nombre + ' ' + this.apellido + ' ' + tel;
+    }
+}
+
+let persona2 = {
+    nombre: 'Nombre2',
+    apellido: 'Apellido2'
+}
+
+console.log(persona1.nombreCompleto('Lic', '122334'));
+
+let arreglo = ['Ing', '124232']; 
+console.log(persona1.nombreCompleto.apply(persona2, arreglo));
